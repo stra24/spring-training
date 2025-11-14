948885f92cb8c9e33ce4309dcc03a4c929ea5a01
junji <test@test.com>
2025-11-15 00:08:51 +0900

    CommentDaoと作成した。

--- src/main/java/com/example/springtraining/repository/CommentRepository.java
+++ src/main/java/com/example/springtraining/repository/CommentRepository.java
@@ -1,7 +1,18 @@
 package com.example.springtraining.repository;
 
+import com.example.springtraining.dao.CommentDao;
 import com.example.springtraining.domain.article.Comment;
-import org.springframework.data.repository.CrudRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Repository;
 
-public interface CommentRepository extends CrudRepository<Comment, Long> {
+@Repository
+@AllArgsConstructor
+public class CommentRepository {
+
+  private final CommentDao dao;
+
+  // 保存する。
+  public void save(Comment comment) {
+    dao.save(comment);
+  }
 }
